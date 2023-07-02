1c586aa0bbedef22c549233bb12f6f5412ffb70b
swastik <93141142+SSahoo2021@users.noreply.github.com>
2023-07-02 11:10:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButtons; i++) {
+  document.querySelector(".drum")[i].addEventListener("click", function () {});
+
+  var buttonInnerHtml = this.innerHtml;
+
+  makeSound(buttonInnerHtml);
+
+  switch (buttonInnerHtml) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+
+    case "s":
+      var tom1 = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+
+    case "d":
+      var tom1 = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+
+    case "j":
+      var tom1 = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+
+    case "k":
+      var tom1 = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+
+    case "l":
+      var tom1 = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+
+    default:
+      console.log;
+  }
+document.addEventListener("keydown", function(Event)
+console.log(Event))
+}
